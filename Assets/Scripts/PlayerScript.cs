3145d5cc20772e4ea425f787db4b33f3ff473d83
Wolfstreak25 <rathodroshan80@gmail.com>
2022-07-11 12:55:57 +0530

    player moves and triggers
    1.Added player movements (velocity)
    2. Created door and portal (triggers)
         door will end the game and portal will move player to some other location
    3. added acceleration later on and removed

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public float speed;
+    public float movespeed=1;
+    //public float Velocity;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Movement Parameters
+       if( Input.GetAxis("Horizontal") > 0 ) //Movement to right
+       {
+        rigidbody2d.velocity =  new Vector2 (speed*movespeed, 0f);
+       }
+       else if( Input.GetAxis("Horizontal") < 0 ) //Movement to left
+       {
+        rigidbody2d.velocity =  new Vector2 (-speed*movespeed, 0f);
+       }
+       else if( Input.GetAxis("Vertical") > 0 ) //Movement to up
+       {
+        rigidbody2d.velocity =  new Vector2 (0f, speed*movespeed);
+       }
+       else if( Input.GetAxis("Vertical") < 0 ) //Movement to down
+       {
+        rigidbody2d.velocity =  new Vector2 (0f, -speed*movespeed);
+       }
+       else if( Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0 ) //Zero Movement
+       {
+        rigidbody2d.velocity =  new Vector2 (0f, 0f);
+       }
+
+    }
+    //Trigger
+       private void OnTriggerEnter2D (Collider2D other) 
+        {
+            if(other.tag == "Door")
+            {
+                Debug.Log("Level Complete"); 
+            }
+            if(other.tag == "Portal")
+            {
+                transform.position = new Vector2(-5.0f, 1.0f);
+            }  
+            if(other.tag == "Portal2")
+            {
+                transform.position = new Vector2(3.0f, -3.0f);
+            }    
+        }
+}
